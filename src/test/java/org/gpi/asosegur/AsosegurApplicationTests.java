5af5081d357dcd7d18fbd0c45f95d98f8ee4c6ad
Luisa Fernanda <fdaluisa96@gmail.com>
2020-10-28 11:51:32 -0500

    Crear proyecto en Spring

---
+++ src/test/java/org/gpi/asosegur/AsosegurApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gpi.asosegur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsosegurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
