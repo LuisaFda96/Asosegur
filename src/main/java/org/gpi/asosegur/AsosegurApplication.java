bce93ce103d40af25044a39397b2722c5273496b
Luisa Fernanda <fdaluisa96@gmail.com>
2020-10-28 11:51:32 -0500

    Crear proyecto en Spring

---
+++ src/main/java/org/gpi/asosegur/AsosegurApplication.java
@@ -0,0 +1,13 @@
+package org.gpi.asosegur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsosegurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsosegurApplication.class, args);
+	}
+
+}
