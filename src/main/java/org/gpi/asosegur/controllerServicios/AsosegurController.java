261bc73d3dfa926e4ccf77965b338b33b2031240
Carlos <carl.rest19@gmail.com>
2020-12-04 13:31:18 -0500

    ajustes vistas

--- src/main/java/org/gpi/asosegur/controllerServicios/AsosegurController.java
+++ src/main/java/org/gpi/asosegur/controllerServicios/AsosegurController.java
@@ -2,8 +2,12 @@ package org.gpi.asosegur.controllerServicios;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@RequestMapping("/asosegur")
+@SessionAttributes("servicios")
 public class AsosegurController {
 
 	@GetMapping("/servicios")
--- src/main/java/org/gpi/asosegur/security/configuration/SecurityConfiguration.java
+++ src/main/java/org/gpi/asosegur/security/configuration/SecurityConfiguration.java
@@ -21,18 +21,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-		
+		return new BCryptPasswordEncoder();  
 	}
 	
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
 		auth.setUserDetailsService(userService);
-		auth.setPasswordEncoder(passwordEncoder());
-		
+		auth.setPasswordEncoder(passwordEncoder()); 
 		return auth;	
 	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.authenticationProvider(authenticationProvider());
@@ -41,7 +40,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeRequests().antMatchers(
-				"/registration**",
+				"/asosegur/**",
+				"/registration/**",
 				"/js/**",
 				"/css/**",
 				"/img/**").permitAll()
