b25943c9b0af51aae0f9189e83d180c795251e20
Carlos <carl.rest19@gmail.com>
2020-12-03 21:01:04 -0500

    .

--- src/main/java/org/gpi/asosegur/controller/UserRegistrationController.java
+++ src/main/java/org/gpi/asosegur/controller/UserRegistrationController.java
@@ -3,7 +3,6 @@ package org.gpi.asosegur.controller;
 import org.gpi.asosegur.dto.UserRegistrationDTO;
 import org.gpi.asosegur.mode.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,32 +11,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/registration")
 public class UserRegistrationController {
-	
-	 private UserService userService;
+
+	private UserService userService;
 
 	public UserRegistrationController(UserService userService) {
 		this.userService = userService;
 	}
+
 	@ModelAttribute("user")
 	public UserRegistrationDTO userRegistrationDTO() {
-		return new UserRegistrationDTO()  ;
-		
+		return new UserRegistrationDTO();
+
 	}
-	
+
 	@GetMapping
 	public String registrationForm() {
-		return "/users/registration";	
+		return "/users/registration";
 	}
+
 //	@GetMapping("/login")
 //	public String login() {
 //		return "/users/login";
 //	}
+
 	@PostMapping
-	public String registerUserAccount(@ModelAttribute("user")  UserRegistrationDTO registrationDTO) {
-	
+	public String registerUserAccount(@ModelAttribute("user") UserRegistrationDTO registrationDTO) {
+
 		userService.save(registrationDTO);
 		return "redirect:/registration?success ";
 	}
-	
 
 }
